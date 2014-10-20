aa3864995c85e4a4ecc89786bad786a8f06de988
xonyne <headfire@bluewin.ch>
2014-10-20 20:29:17 +0200

    Database access class
    Read functions for entities

---
+++ application/models/db/Category.php
@@ -0,0 +1,44 @@
+﻿<?php
+namespace Application\Models\Db;
+
+/**
+ * ****************************************************************************
+ * Category class - represents a article category. Can have a parent category.
+ * ****************************************************************************
+ */
+class Category {
+	private $id;
+	private $name;
+	private $parentId;
+	
+	public function __construct($id, $name, $parentId) {
+		$this->id = $id;
+		$this->name = $name;
+		$this->parentId = $parentId;
+	}
+	
+	// ------------------------ GETTER ---------------------------- //
+	
+	public function getId () {
+		return $this->id;
+	}
+	
+	public function getName () {
+		return $this->name;
+	}
+	
+	public function getParentId () {
+		return $this->parentId;
+	}
+	
+	// ------------------------ SETTER ---------------------------- //
+	
+	public function setName ($name) {
+		$this->name=$name;
+	}
+	
+	public function setParentId ($parentId) {
+		$this->parentId=$parentId;
+	}
+}
+?>
---
+++ tests/DBAccessTest.php
@@ -0,0 +1,24 @@
+﻿<?php
+use Application\Models\Db\DBAccess;
+
+$dbAccess = new DBAccess();
+
+$article = $dbAccess->getArticleById(4);
+printObjectArray('getArticleById', array($article));
+
+$articles = $dbAccess->getAllArticles();
+printObjectArray('getAllArticles', $articles);
+
+$location = $dbAccess->getLocationById(1);
+printObjectArray('getLocationById', array($location));
+
+$category = $dbAccess->getCategoryById(1);
+printObjectArray('getCategoryById', array($category));
+	
+function printObjectArray($functionName, $objectArr) {
+	echo '<h1>' . $functionName . '</h1>';
+	foreach($objectArr as $obj){
+		print_r($obj);
+	}
+}
+?>
