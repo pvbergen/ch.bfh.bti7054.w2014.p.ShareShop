99811d35a7ca8ccd88d09803379f481e66f5e229
pvbergen <philippe@vonbergen.net>
2014-11-07 11:43:08 +0100

    Layout, Header

--- public/js/js.js
+++ public/js/js.js
@@ -1,5 +1,13 @@
 $( document ).ready(function() {
     
+	$(window).on('scroll', function() {
+		if($(window).scrollTop() > 0) {
+			$('#header').addClass('fixed');
+		} else {
+			$('#header').removeClass('fixed');	
+		}
+	});
+	
 	/*
 		Navigation Toggle
 	*/
