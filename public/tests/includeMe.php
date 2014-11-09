2c6489bc2d1c68a76db2e7e01da13431c2784540
xonyne <headfire@bluewin.ch>
2014-11-09 22:03:08 +0100

    Moved tests to public folder

--- public/tests/includeMe.php
+++ public/tests/includeMe.php
@@ -1,12 +1,12 @@
 <?php
 mb_internal_encoding('UTF-8');
-define("BASE_PATH", dirname(dirname(__FILE__)));
+define("BASE_PATH", dirname(dirname(dirname(__FILE__))));
 define("APPLICATION_PATH", BASE_PATH . '\application');
 define("APPLICATION_ENV", 'development');
 
-require_once BASE_PATH . '/lib/SplClassLoader.php';
+require_once BASE_PATH . '\lib\SplClassLoader.php';
 
-$autoloader = new SplClassLoader('Shareshop', BASE_PATH . '/lib');
+$autoloader = new SplClassLoader('Shareshop', BASE_PATH . 'lib');
 $autoloader->register();
 
 $autoloader = new SplClassLoader('Application', BASE_PATH);
