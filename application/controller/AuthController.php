f5f0036ddb854a80343ed647bbb29260cc37d25f
xonyne <headfire@bluewin.ch>
2015-01-09 16:25:42 +0100

    Übeprüfe auf vorhandenen Username bei Registrierung

--- application/controller/AuthController.php
+++ application/controller/AuthController.php
@@ -57,15 +57,20 @@ class AuthController extends \Shareshop\Controller {
 				if (preg_match('/[^@]{1,63}@[^\.]{1,63}\.[a-zA-Z0-9]{2,63}/', $postData['email'])) {
 					if ($this->checkAddress($postData['adresse'])) {
 						if ($postData['password'] == $postData['passwordRepeat']) {
-							$salt = $this->generateString(10);
-							$password = $this->createHash($postData['password']);//, $salt);
-							User::create()->setUsername($postData['username'])->setPassword($password)->setEmail($postData['email'])->setSalt($salt)->setState(0)->save();
-							$address = split(',',$postData['adresse']);
-							$street = trim($address[0]);
-							$plz = split(' ', trim($address[1]))[0];
-							$town = split(' ', trim($address[1]))[1];
-							Location::create()->setStreet($street)->setPostcode($plz)->setTown($town)->setMapLat($postData['adresse_lat'])->setMapLng($postData['adresse_lng'])->save();
-							$viewData['success'] = 'Erfolgreich registriert!';
+							$userDB = User::findByUsername($postData['username']);
+							if ($userDB == null || $userDB->getUsername() != strtolower($postData['username'])) {
+								$salt = $this->generateString(10);
+								$password = $this->createHash($postData['password']);//, $salt);
+								User::create()->setUsername($postData['username'])->setPassword($password)->setEmail($postData['email'])->setSalt($salt)->setState(0)->save();
+								$address = split(',',$postData['adresse']);
+								$street = trim($address[0]);
+								$plz = split(' ', trim($address[1]))[0];
+								$town = split(' ', trim($address[1]))[1];
+								Location::create()->setStreet($street)->setPostcode($plz)->setTown($town)->setMapLat($postData['adresse_lat'])->setMapLng($postData['adresse_lng'])->save();
+								$viewData['success'] = 'Erfolgreich registriert!';
+							} else {
+								$viewData['error'] = 'Dieser Username existiert bereits.';
+							}
 						} else {
 							$viewData['error'] = 'Passwörter stimmen nicht überein.';
 						}
